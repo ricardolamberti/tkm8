39e613879a0b3c8a81fd78e8c181a3802572442a
ricardolamberti <44089144+ricardolamberti@users.noreply.github.com>
2025-08-11 16:29:56 -0300

    refactor: update EvaluateDetail form

--- src/pss/bsp/contrato/evaluate/detail/FormEvaluateDetail.java
+++ src/pss/bsp/contrato/evaluate/detail/FormEvaluateDetail.java
@@ -1,51 +1,25 @@
 package pss.bsp.contrato.evaluate.detail;
 
-import java.awt.Dimension;
-import java.awt.Rectangle;
-
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
-
-import pss.bsp.contrato.detalle.BizDetalle;
-import pss.bsp.contrato.detalle.nivel.BizNivel;
-import pss.bsp.contrato.logica.JContratoNormal;
-import pss.bsp.event.GuiBSPSqlEvents;
-import pss.common.event.sql.GuiSqlEvents;
-import pss.common.regions.company.GuiCompanies;
-import pss.common.regions.divitions.GuiPaisesLista;
-import pss.core.ui.components.JPssEdit;
-import pss.core.ui.components.JPssHtmlTextArea;
-import pss.core.ui.components.JPssLabel;
-import pss.core.ui.components.JPssLabelFormLov;
-import pss.core.ui.components.JPssLabelWinLov;
-import pss.core.win.JControlWin;
 import pss.core.win.JWin;
-import pss.core.win.JWins;
-import pss.core.winUI.controls.JControlCombo;
-import pss.core.winUI.controls.JFormControl;
 import pss.core.winUI.forms.JBaseForm;
-import pss.core.winUI.icons.GuiIconos;
 import pss.core.winUI.responsiveControls.JFormPanelResponsive;
-import pss.tourism.carrier.GuiCarriers;
 
 public class FormEvaluateDetail extends JBaseForm {
 
+  private static final long serialVersionUID = 1477827540739L;
 
-private static final long serialVersionUID = 1477827540739L;
-
-  /**
-   * Propiedades de la Clase
-   */
-
-
-
-  /**
-   * Constructor de la Clase
-   */
   public FormEvaluateDetail() throws Exception {
-   }
+  }
 
   public GuiEvaluateDetail getWin() { return (GuiEvaluateDetail) getBaseWin(); }
 
- 
-}  //  @jve:decl-index=0:visual-constraint="14,3"
+  @Override
+  public void InicializarPanel(JWin zWin) throws Exception {
+    AddItemEdit(null, CHAR, OPT, "id").setHide(true);
+    JFormPanelResponsive row = AddItemRow();
+    row.AddItemCheck("Ok", OPT, "ok").setSizeColumns(2);
+    row.AddItemEdit("Number", UINT, OPT, "number").setSizeColumns(2);
+    row.AddItemEdit("Result", CHAR, OPT, "result").setSizeColumns(8);
+    AddItemRow().AddItemArea("Detail", CHAR, OPT, "detail").setHeight(80);
+  }
+}
