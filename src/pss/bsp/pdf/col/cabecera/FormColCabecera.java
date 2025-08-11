04b65d61a95711ed66fb48c4d0876993aac170f2
ricardolamberti <44089144+ricardolamberti@users.noreply.github.com>
2025-08-11 17:49:55 -0300

    refactor: migrate cabecera forms to responsive model

--- src/pss/bsp/pdf/col/cabecera/FormColCabecera.java
+++ src/pss/bsp/pdf/col/cabecera/FormColCabecera.java
@@ -8,31 +8,33 @@ public class FormColCabecera extends JBaseForm {
 
   private static final long serialVersionUID = 1245253623694L;
 
-  public FormColCabecera() throws Exception {
-  }
+  public FormColCabecera() throws Exception {}
 
-  public GuiColCabecera getWin() {
-    return (GuiColCabecera) getBaseWin();
-  }
+  public GuiColCabecera getWin() { return (GuiColCabecera) getBaseWin(); }
 
-  public void InicializarPanel(JWin zWin) throws Exception {
-    AddItemEdit(null, CHAR, REQ, "company").setHide(true);
-    AddItemEdit(null, CHAR, REQ, "owner").setHide(true);
+  /**
+   * Linkeo los campos con la variables del form
+   */
+  public void InicializarPanel( JWin zWin ) throws Exception {
+    AddItemEdit( null, CHAR, REQ, "company" ).setHide(true);
+    AddItemEdit( null, CHAR, REQ, "owner" ).setHide(true);
 
     JFormPanelResponsive row = AddItemRow();
-    row.AddItemEdit("ID Liquidacion", CHAR, REQ, "idPDF").setSizeColumns(6);
-    row.AddItemEdit("Descripcion", CHAR, REQ, "descripcion").setSizeColumns(6);
+    row.AddItemEdit( "ID Liquidacion", CHAR, REQ, "idPDF" ).setSizeColumns(6);
+    row.AddItemEdit( "IATA", CHAR, REQ, "IATA" ).setSizeColumns(6);
 
     row = AddItemRow();
-    row.AddItemEdit("Direccion", CHAR, REQ, "direccion").setSizeColumns(6);
-    row.AddItemEdit("Codigo postal", CHAR, REQ, "codigo_postal").setSizeColumns(6);
+    row.AddItemEdit( "Descripcion", CHAR, REQ, "descripcion" ).setSizeColumns(12);
 
     row = AddItemRow();
-    row.AddItemEdit("IATA", CHAR, REQ, "IATA").setSizeColumns(6);
-    row.AddItemEdit("Clave Fiscal", CHAR, REQ, "CIF").setSizeColumns(6);
+    row.AddItemEdit( "Direccion", CHAR, REQ, "direccion" ).setSizeColumns(6);
+    row.AddItemEdit( "Codigo postal", CHAR, REQ, "codigo_postal" ).setSizeColumns(6);
 
     row = AddItemRow();
-    row.AddItemEdit("Localidad", CHAR, REQ, "localidad").setSizeColumns(6);
-    row.AddItemEdit("Moneda", CHAR, REQ, "moneda").setSizeColumns(6);
+    row.AddItemEdit( "Localidad", CHAR, REQ, "localidad" ).setSizeColumns(6);
+    row.AddItemEdit( "Clave Fiscal", CHAR, REQ, "CIF" ).setSizeColumns(6);
+
+    row = AddItemRow();
+    row.AddItemEdit( "Moneda", CHAR, REQ, "moneda" ).setSizeColumns(6);
   }
 }
