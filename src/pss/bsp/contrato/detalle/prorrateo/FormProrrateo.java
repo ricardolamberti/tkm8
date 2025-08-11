c0aa5465b9dc75c239dd516b7d400b2c96d19c27
ricardolamberti <44089144+ricardolamberti@users.noreply.github.com>
2025-08-11 15:52:53 -0300

    Convert old contrato forms to responsive layout

--- src/pss/bsp/contrato/detalle/prorrateo/FormProrrateo.java
+++ src/pss/bsp/contrato/detalle/prorrateo/FormProrrateo.java
@@ -1,6 +1,11 @@
 package pss.bsp.contrato.detalle.prorrateo;
 
+import pss.bsp.dk.GuiClienteDKs;
+import pss.core.win.JControlWin;
+import pss.core.win.JWin;
+import pss.core.win.JWins;
 import pss.core.winUI.forms.JBaseForm;
+import pss.core.winUI.responsiveControls.JFormPanelResponsive;
 
 public class FormProrrateo extends JBaseForm {
 
@@ -19,5 +24,31 @@ private static final long serialVersionUID = 1446860278358L;
   }
 
   public GuiProrrateo getWin() { return (GuiProrrateo) getBaseWin(); }
+
+  /**
+   * Linkeo los campos con la variables del form
+   */
+  public void InicializarPanel( JWin zWin ) throws Exception {
+        AddItemEdit( null, CHAR, OPT, "id_prorrateo" ).setHide(true);
+        AddItemEdit( null, CHAR, OPT, "company" ).setHide(true);
+        AddItemEdit( null, CHAR, OPT, "contrato" ).setHide(true);
+        AddItemEdit( null, CHAR, OPT, "detalle" ).setHide(true);
+
+        JFormPanelResponsive row = AddItemRow();
+        row.AddItemWinLov( "Cliente", CHAR, REQ, "cliente", new JControlWin() {
+                @Override
+                public JWins getRecords(boolean bOneRow) throws Exception {
+                        return getClientes(bOneRow);
+                }
+        }).setSizeColumns(8);
+        row.AddItemEdit( "Comision", FLOAT, REQ, "valor" ).setSizeColumns(4);
+  }
+
+  public JWins getClientes(boolean one) throws Exception {
+        GuiClienteDKs wins = new GuiClienteDKs();
+        wins.getRecords().addFilter("company", getWin().GetcDato().getCompany());
+        wins.getRecords().addOrderBy("dk");
+        return wins;
+  }
 
 }  //  @jve:decl-index=0:visual-constraint="7,4"
--- src/pss/bsp/contrato/total/FormTotal.java
+++ src/pss/bsp/contrato/total/FormTotal.java
@@ -1,6 +1,8 @@
 package pss.bsp.contrato.total;
 
+import pss.core.win.JWin;
 import pss.core.winUI.forms.JBaseForm;
+import pss.core.winUI.responsiveControls.JFormPanelResponsive;
 
 public class FormTotal extends JBaseForm {
 
@@ -16,5 +18,19 @@ private static final long serialVersionUID = 1446860154249L;
 
   public GuiTotal getWin() { return (GuiTotal) getBaseWin(); }
 
-  
-} 
+  /**
+   * Linkeo los campos con la variables del form
+   */
+  public void InicializarPanel( JWin zWin ) throws Exception {
+        AddItemEdit( null, CHAR, OPT, "id" ).setHide(true);
+
+        JFormPanelResponsive row = AddItemRow();
+        row.AddItemEdit( "Grupo", CHAR, OPT, "grupo" ).setReadOnly(true).setSizeColumns(6);
+        row.AddItemEdit( "Moneda", CHAR, OPT, "moneda" ).setReadOnly(true).setSizeColumns(6);
+
+        row = AddItemRow();
+        row.AddItemEdit( "Base Comisionable", FLOAT, OPT, "base_comisionable" ).setReadOnly(true).setSizeColumns(6);
+        row.AddItemEdit( "Ganancia", FLOAT, OPT, "ganancia" ).setReadOnly(true).setSizeColumns(6);
+  }
+
+}
